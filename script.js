719dca9d52f3da30a801eb99ca4c1b2f4bfa161c
Stanley Ho <55599499+sirsavant@users.noreply.github.com>
2020-09-03 17:53:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 // data we can work with.
 const inventors = [
   { first: "Albert", last: "Einstein", year: 1879, passed: 1955 },
@@ -14,17 +13,62 @@ const people = ["Beck, Glenn", "Becker, Carl", "Beckett, Samuel", "Beddoes, Mick
 
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1500's
-const filter = inventors.filter(checkInventors);
 
-function checkInventors() {
-  
-}
+// Method 1: without the filter method.
+
+// create variable called inventors1500 that is an empty array.
+// use the for loop to find out which element in the array has a year value greater than or equal to 1500 and less than 1600. If there is, we will use push() to push the inventor element to the inventors1500 array.
+
+/*
+let inventors1500 = [];
+
+  for (i = 0; i < inventors.length; i++) {
+    if (inventors[i].year >= 1500 && inventors[i].year < 1600) {
+      inventors1500.push(inventors[i]);
+    }
+  }
+
+console.log(inventors1500);
+*/
+
+// Method two: using the filter method
+let inventors1500 = inventors.filter(function(e) {
+  return e.year > 1500 && e.year < 1600;
+});
+
+console.log(inventors1500);
+
+// Method 3: using ES6 arrow function
+/*
+let inventors1500 = inventors.filter(e => e.year < 1500);
+
+console.log(inventors1500);
+*/
+
+// https://www.javascripttutorial.net/javascript-array-filter/
 
 // Array.prototype.map()
-// 2. Give us an array of the inventory first and last names
+// 2. Give us an array of the inventory first and last names.
+
+//array map() method allows you to call a function to modify each item in the array.
+
+let inventorsFandL = inventors.map(function(e) {
+  return `${e.first} ${e.last}`;
+});
+
+console.log(inventorsFandL);
+
 
 // Array.prototype.sort()
 // 3. Sort the inventors by birthdate, oldest to youngest
+
+// https://www.w3schools.com/js/js_array_sort.asp
+
+let sortInventors = inventors.sort(function(a, b) { 
+  return (a.year - b.year);
+});
+
+console.log(sortInventors);
 
 // Array.prototype.reduce()
 // 4. How many years did all the inventors live?
